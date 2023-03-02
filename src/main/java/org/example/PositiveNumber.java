eceb5ad3a0d2ab5deb5a8231c51055b25acd0ac6
Lapoh <valeriu.masnic@gmail.com>
2023-03-02 23:51:13 +0200

    first 4 exercises

--- src/main/java/org/example/PositiveNumber.java
+++ src/main/java/org/example/PositiveNumber.java
@@ -1,2 +1,28 @@
-package org.example;public class PositiveNumber {
+package org.example;
+
+import java.util.Scanner;
+
+public class PositiveNumber {
+    static void checkOnePositiveIntVarOne() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        if (x <= 0 && y <= 0 && z > 0) {
+            System.out.println("true");
+        } else if (x <= 0 && y > 0 && z <= 0) {
+            System.out.println("true");
+        } else if (x > 0 && y <= 0 && z <= 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkOnePositiveIntVarOne();
+    }
 }
+
+
--- src/main/java/org/example/SameNumers.java
+++ src/main/java/org/example/SameNumers.java
@@ -1,2 +1,34 @@
-package org.example;public class SameNumers {
+package org.example;
+
+import java.util.Scanner;
+
+public class SameNumers {
+    static void checkTheSameNumbersVarOne() {
+        //Ex. 1
+        //Var.1
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        if (x == y || x == z || y == z) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+    }
+
+    static void checkTheSameNumberVarTwo() {
+        //Var.2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        String result = (x == y || x == z || y == z) ? "false" : "true";
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+
+    }
 }
